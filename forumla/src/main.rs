d4eae9338779d56c7597df81a77ad16fc48061dd
Jon Gjengset <jon@thesquareplanet.com>
2020-06-22 13:59:03 -0400

    eval: access patterns

--- forumla/src/main.rs
+++ forumla/src/main.rs
@@ -1,41 +1,48 @@
 const NUM: usize = 5000000;
-const RATE: usize = 800000;
-const EXP: f64 = 1.08;
 
 fn main() {
-    for &t in &[32, 64, 128, 256, 512] {
-        println!("after {}s", t);
-        let integral_w = est(t, 0.05);
-        println!(
-            ".. {}/{} articles voted on ({:.1}%)",
-            integral_w as usize,
-            NUM,
-            100.0 * (integral_w / (NUM as f64))
-        );
+    for &(skew, exp) in &[("80/20", 0.886), ("80/5", 0.99), ("90/1", 1.15)] {
+        println!("\\hline");
 
-        let integral_r = est(t, 0.95);
-        println!(
-            ".. {}/{} articles read ({:.1}%)",
-            integral_r as usize,
-            NUM,
-            100.0 * (integral_r / (NUM as f64))
-        );
-    }
+        let mut first = true;
+        let harmonic = harmonic(NUM, exp);
+        for &rate in &[100_000, 1_000_000, 10_000_000] {
+            let pct = |pt| {
+                let mut p = 0.0;
+                let mut k = 1;
+                while p < pt && k <= NUM {
+                    p += zipf(k, exp, harmonic);
+                    k += 1;
+                }
+                // println!(
+                //     "the first {} articles ({:.1}%) make up {:.1}% of requests",
+                //     k,
+                //     100.0 * k as f64 / NUM as f64,
+                //     100.0 * p
+                // );
+                100.0 * k as f64 / NUM as f64
+            };
 
-    for &pt in &[0.9, 0.95, 0.99] {
-        let mut p = 0.0;
-        let mut k = 1;
-        let harmonic = harmonic(NUM, EXP);
-        while p < pt && k <= NUM {
-            p += zipf(k, EXP, harmonic);
-            k += 1;
+            // let eighty_p = pct(0.8);
+            let nines_p = pct(0.99);
+            let one_s = 100.0 * (est(1, rate, exp) / (NUM as f64));
+            let thirty_s = 100.0 * (est(30, rate, exp) / (NUM as f64));
+
+            let human_rate = format!("{}M/s", rate as f64 / 1_000_000.0);
+
+            if first {
+                println!(
+                    "{} & {:.2} & {} & {:.1} & {:.1} & {:.1} \\\\",
+                    skew, exp, human_rate, nines_p, one_s, thirty_s
+                );
+            } else {
+                println!(
+                    "& & {} & {:.1} & {:.1} & {:.1} \\\\",
+                    human_rate, nines_p, one_s, thirty_s
+                );
+            }
+            first = false;
         }
-        println!(
-            "the first {} articles ({:.1}%) make up {:.1}% of requests",
-            k,
-            100.0 * k as f64 / NUM as f64,
-            100.0 * p
-        );
     }
 }
 
@@ -48,9 +55,9 @@ fn zipf(k: usize, s: f64, harmonic: f64) -> f64 {
     (1.0 / (k as f64).powf(s)) / harmonic
 }
 
-fn est(t: usize, fraction: f64) -> f64 {
-    let harmonic = harmonic(NUM, EXP);
+fn est(t: usize, rate: usize, exp: f64) -> f64 {
+    let harmonic = harmonic(NUM, exp);
     (1..=NUM)
-        .map(|k| 1.0 - (1.0 - zipf(k, EXP, harmonic)).powf((t * RATE) as f64 * fraction))
+        .map(|k| 1.0 - (1.0 - zipf(k, exp, harmonic)).powf((t * rate) as f64))
         .sum()
 }
